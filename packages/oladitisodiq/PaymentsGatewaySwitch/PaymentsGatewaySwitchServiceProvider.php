4ed89f4723e934b0708730f99439e55f4bd28073
oladitisodiq <oladitisodiq@email.com>
2024-10-18 09:57:21 +0100

    testing my dd to see if packages works

---
+++ packages/oladitisodiq/PaymentsGatewaySwitch/PaymentsGatewaySwitchServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PaymentsGatewaySwitch;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class PaymentsGatewaySwitchServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+       
+        
+   
+    }
+
+    public function boot()
+    {
+        dd('it works well done ');
+    }
+}
