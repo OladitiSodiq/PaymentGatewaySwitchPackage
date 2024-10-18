e2f41a47f97390f972fc1c1f6addc65dff7be752
oladitisodiq <oladitisodiq@email.com>
2024-10-18 10:54:19 +0100

    Added  Controller and routes/api for testing through postman

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PaymentsGatewaySwitch\Facades\PaymentsGatewaySwitch;
+
+class PaymentController extends Controller
+{
+    //
+    public function processPayment(Request $request)
+    {
+
+        // dd("isisjs");
+        $data = [
+            'amount' => $request->input('amount'),
+            'location' => $request->input('location'),
+          
+            
+        ];
+
+        try {
+            $response = PaymentsGatewaySwitch::pay($data);
+            return response()->json($response);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 /*
@@ -18,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/pay', [PaymentController::class, 'processPayment']);
